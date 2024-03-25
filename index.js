c52acd2faed12c99c02424574af0c0d945ffe5f8
mohammed <mohammed.el-boudi@aurum-consulting.de>
2024-03-25 11:35:15 +0100

    test

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+app.get("/", function (req, res) {
+  res.send('{ "response": "Hello From Thetips4you" }');
 });
 
-app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+app.get("/will", function (req, res) {
+  res.send('{ "response": "Hello World great" }');
 });
-app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+app.get("/ready", function (req, res) {
+  res.send('{ "response": " Great!, It works!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
